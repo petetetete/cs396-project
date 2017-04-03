62c0e9f3765a24fe26cc7d709363499d4ae2d024
petetetete <huettlpeter@gmail.com>
2017-04-02 23:25:44 -0700

    Improve names and parameters

--- gen_rnd_expr.scm
+++ gen_rnd_expr.scm
@@ -15,29 +15,35 @@
           (let ((func (vector-ref funcs (floor (rand (vector-length funcs))))))
             (cons (car func) (get_arguments (cdr func))))))))
 
-(define gen_init_pop
-  (lambda (size rand funcs terms maxd method)
-    (if (= size 0)
+(define gen_rnd_exprs
+  (lambda (count)
+    (if (= count 0)
         empty
-        (cons (gen_rnd_expr rand funcs terms maxd method) (gen_init_pop (- size 1) rand funcs terms maxd method)))))
+        (cons
+         (gen_rnd_expr random functions terminals max_depth method)
+         (gen_rnd_exprs (- count 1))))))
 
 (define fitness
-  (lambda (goal expr)
+  (lambda (expr)
     (with-handlers ([exn:fail:contract:divide-by-zero? (lambda (exn) 0)])
       (let ((val (eval expr)))
         (if (= val 42)
             1
             (/ 1 (abs (- 42 val))))))))
 
+
 ; Tests
 (define goal 42)
 (define pop_size 20)
 (define to_kill 5)
+(define max_depth 4)
+(define method 'full)
+(define max_iter 50)
 (define functions (vector '( + . 2) '( - . 2) '( * . 2 ) '( / . 2 )))
 (define terminals (vector 1 2 5 10))
 
-(define population (gen_init_pop pop_size random functions terminals 3 'full))
+(define initial_pop (gen_rnd_exprs pop_size))
 
+(fitness (car (sort initial_pop (lambda (a b) (> (fitness a) (fitness b))))))
 
-(fitness goal (car population))
 
