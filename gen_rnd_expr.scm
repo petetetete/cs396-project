1a3f395e066a49590aa061585354d85cd82d136d
petetetete <huettlpeter@gmail.com>
2017-04-02 23:15:43 -0700

    Add fitness function and population generator

--- gen_rnd_expr.scm
+++ gen_rnd_expr.scm
@@ -1,3 +1,5 @@
+(load "list.scm")
+
 ; gen_rnd_expr: procedure vector vector positive-integer symbol -> list
 ; generate an initial population of programs using
 ; vectors of functions and terms
@@ -13,28 +15,29 @@
           (let ((func (vector-ref funcs (floor (rand (vector-length funcs))))))
             (cons (car func) (get_arguments (cdr func))))))))
 
+(define gen_init_pop
+  (lambda (size rand funcs terms maxd method)
+    (if (= size 0)
+        empty
+        (cons (gen_rnd_expr rand funcs terms maxd method) (gen_init_pop (- size 1) rand funcs terms maxd method)))))
 
+(define fitness
+  (lambda (goal expr)
+    (with-handlers ([exn:fail:contract:divide-by-zero? (lambda (exn) 0)])
+      (let ((val (eval expr)))
+        (if (= val 42)
+            1
+            (/ 1 (abs (- 42 val))))))))
 
 ; Tests
-(define (iflt e1 e2 then else)  (quote (if (< e1 e2) then else)))                
-(define functions (vector '( + . 2) '( - . 2) '( * . 2 ) '( / . 2 ) '(iflt . 4)))
-(define terminals (vector 'x 'y 1 5 10))
+(define goal 42)
+(define pop_size 20)
+(define to_kill 5)
+(define functions (vector '( + . 2) '( - . 2) '( * . 2 ) '( / . 2 )))
+(define terminals (vector 1 2 5 10))
 
-(define side #f)
-(define last -1)
-(define not_random
-  (lambda args
-    (if (null? args)
-        (if side 0.0 (begin (set! side (not side)) 0.99))
-        (let ((bound (car args)))
-          (begin (set! last (modulo (+ 1 last) bound))
-                 last)))))
+(define population (gen_init_pop pop_size random functions terminals 3 'full))
 
-(display "Not random tests\n")
-(gen_rnd_expr not_random functions terminals 2 'grow)
-(gen_rnd_expr not_random functions terminals 2 'grow)
-(gen_rnd_expr not_random functions terminals 2 'full)
 
-(display "\nRandom tests\n")
-(gen_rnd_expr random functions terminals 2 'grow)
-(gen_rnd_expr random functions terminals 3 'full)
+(fitness goal (car population))
+
