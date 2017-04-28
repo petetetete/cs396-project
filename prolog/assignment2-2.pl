a87b150cedbbda5cb783f144dc272fc056c3a7e9
petetetete <huettlpeter@gmail.com>
2017-04-28 09:34:40 -0700

    Improve value false checks and documentation comments

--- prolog/assignment2-2.pl
+++ prolog/assignment2-2.pl
@@ -1,19 +1,24 @@
 % member/2
-% is true if and only if an element is
-% in a list
+% is true if and only if an
+% element is in a list
 member(X, cons(X, _Z)).
 member(X, cons(_Y, Z)) :- member(X, Z).
 
 % get/3
-% is true if and only if an element in L
-% matches the pair(K, V)
+% is true if and only if an element 
+% in L matches the pair(K, V)
 get(K, L, V) :- member(pair(K, V), L).
 
 % value/3
+% evaluate the truthiness of an expression given
+% the assignments in L and output into V
 value(F, L, V) :- atom(F), get(F, L, V).
+
 value(con(X, Y), L, true) :- value(X, L, true), value(Y, L, true).
-value(con(X, Y), L, false).
+value(con(X, Y), L, false) :- value(X, L, false); value(Y, L, false).
+
 value(dis(X, Y), L, true) :- value(X, L, true); value(Y, L, true).
-value(dis(X, Y), L, false).
+value(dis(X, Y), L, false) :- value(X, L, false), value(Y, L, false).
+
 value(imp(X, Y), L, true) :- value(X, L, false); value(Y, L, true).
-value(imp(X, Y), L, false).
+value(imp(X, Y), L, false) :- value(X, L, true), value(Y, L, false).
